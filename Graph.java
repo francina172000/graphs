996d73c4555045bd9674ef0ef178e115b3635e3a
Kate Holdener <kate.holdener@slu.edu>
2023-03-07 22:10:40 -0600

    updated code

--- Graph.java
+++ Graph.java
@@ -1,63 +1,80 @@
+// Java program to print BFS traversal from a given source
+// vertex. BFS(int s) traverses vertices reachable from s.
 import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.function.*;
-import java.util.regex.*;
-import java.util.stream.*;
-import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.toList;
 
+// This class represents a directed graph using adjacency
+// list representation
 class Graph {
+	private int V; // No. of vertices
+	private LinkedList<Integer> adj[]; // Adjacency Lists
 
-    /*
-     * Complete the 'bfs' function below.
-     *
-     * The function is expected to return an INTEGER_ARRAY.
-     * The function accepts following parameters:
-     *  1. INTEGER n
-     *  2. INTEGER m
-     *  3. 2D_INTEGER_ARRAY edges
-     */
+	// Constructor
+	Graph(int v)
+	{
+		V = v;
+		adj = new LinkedList[v];
+		for (int i = 0; i < v; ++i)
+			adj[i] = new LinkedList<Integer>();
+	}
 
-    public static void bfs(int n, int m, List<List<Integer>> edges) {
-    // Write your code here
+	// Function to add an edge into the graph
+	void addEdge(int v, int w) { adj[v].add(w); }
 
-    }
+	// prints BFS traversal from a given source s
+	void BFS(int s)
+	{
+		// Mark all the vertices as not visited(By default
+		// set as false)
+		boolean visited[] = new boolean[V];
 
-    public static void main(String[] args) throws IOException {
-         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		// Create a queue for BFS
+		LinkedList<Integer> queue
+			= new LinkedList<Integer>();
 
-         try {
-             String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+		// Mark the current node as visited and enqueue it
+		visited[s] = true;
+		queue.add(s);
 
-             int n = Integer.parseInt(firstMultipleInput[0]);
-             int m = Integer.parseInt(firstMultipleInput[1]);
+		while (queue.size() != 0) {
+			// Dequeue a vertex from queue and print it
+			s = queue.poll();
+			System.out.print(s + " ");
 
-             List<List<Integer>> edges = new ArrayList<>();
+			// Get all adjacent vertices of the dequeued
+			// vertex s If a adjacent has not been visited,
+			// then mark it visited and enqueue it
+			Iterator<Integer> i = adj[s].listIterator();
+			while (i.hasNext()) {
+				int n = i.next();
+				if (!visited[n]) {
+					visited[n] = true;
+					queue.add(n);
+				}
+			}
+		}
+	}
 
-             IntStream.range(0, m).forEach(i -> {
-                 try {
-                     edges.add(
-                         Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
-                             .map(Integer::parseInt)
-                             .collect(toList())
-                     );
-                 } catch (IOException ex) {
-                     throw new RuntimeException(ex);
-                 }
-             });
+   void DFS(int s){
+   }
 
+	// Driver method to
+	public static void main(String args[])
+	{
+		Graph g = new Graph(4);
 
-             Graph.bfs(n, m, edges);
+		g.addEdge(0, 1);
+		g.addEdge(0, 2);
+		g.addEdge(1, 2);
+		g.addEdge(2, 0);
+		g.addEdge(2, 3);
+		g.addEdge(3, 3);
 
-          } 
-          catch (IOException ex) {
-             throw new RuntimeException(ex);
-        }
-        bufferedReader.close();
-    }
+		System.out.println(
+			"Following is Breadth First Traversal "
+			+ "(starting from vertex 2)");
+
+		g.BFS(2);
+	}
 }
 
--- graph.py
+++ graph.py
@@ -1,34 +1,68 @@
-import math
-import os
-import random
-import re
-import sys
+# Python3 Program to print BFS traversal
+# from a given source vertex. BFS(int s)
+# traverses vertices reachable from s.
 
-#
-# Complete the 'bfs' function below.
-#
-# The function is expected to return an INTEGER_ARRAY.
-# The function accepts following parameters:
-#  1. INTEGER n
-#  2. INTEGER m
-#  3. 2D_INTEGER_ARRAY edges
-#
-
-def bfs(n, m, edges):
-    # Write your code here
-    print('bfs')
-
-if __name__ == '__main__':
-     first_multiple_input = input().rstrip().split()
-
-     n = int(first_multiple_input[0])
-
-     m = int(first_multiple_input[1])
-
-     edges = []
-
-     for _ in range(m):
-        edges.append(list(map(int, input().rstrip().split())))
+# This class represents a directed graph
+# using adjacency list representation
 
 
-     bfs(n, m, edges)
+class Graph:
+
+   # Constructor
+   def __init__(self):
+
+      # default dictionary to store graph
+      self.graph = {}
+
+   # function to add an edge to graph
+   def addEdge(self, u, v):
+      if not u in self.graph:
+         self.graph[u] = [v]
+      else:
+         self.graph[u].append(v)
+
+   # Function to print a BFS of graph
+   def BFS(self, s):
+      # Mark all the vertices as not visited
+      visited = [False] * (max(self.graph) + 1)
+
+      # Create a queue for BFS
+      queue = []
+
+      # Mark the source node as
+      # visited and enqueue it
+      queue.append(s)
+      visited[s] = True
+
+      while queue:
+
+         # Dequeue a vertex from
+         # queue and print it
+         s = queue.pop(0)
+         print(s, end=" ")
+
+         # Get all adjacent vertices of the
+         # dequeued vertex s. If a adjacent
+         # has not been visited, then mark it
+         # visited and enqueue it
+         for i in self.graph[s]:
+            if visited[i] == False:
+               queue.append(i)
+               visited[i] = True
+
+# Driver code
+
+
+# Create a graph given in
+# the above diagram
+g = Graph()
+g.addEdge(0, 1)
+g.addEdge(0, 2)
+g.addEdge(1, 2)
+g.addEdge(2, 0)
+g.addEdge(2, 3)
+g.addEdge(3, 3)
+
+print("Following is Breadth First Traversal"
+   " (starting from vertex 2)")
+g.BFS(2)
